5210518616a9ae474e5ec5b8f985549bc90c52c3
burakizm <88186150+burakizm@users.noreply.github.com>
2021-08-30 19:05:58 +0300

    start.ts

---
+++ kaynak/bot/işleyiciler/start.ts
@@ -0,0 +1,7 @@
+{ Besteci } 'grammy'den içe aktar;
+
+const besteci = yeni Besteci();
+
+varsayılan oluşturucuyu dışa aktar;
+
+composer.command('start', (ctx) => ctx.reply('Merhaba ben GSM müzik.Komutlar ve yardım için @leynfay , @burakizm'));
